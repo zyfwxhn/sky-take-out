72c05182581ee70996b9d2d7286b6bc3ff10439e
darling <3119859920@qq.com>
2026-02-13 22:37:52 +0800

    新增员工功能

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -68,6 +69,14 @@ public class EmployeeController {
      */
     @PostMapping("/logout")
     public Result<String> logout() {
+        return Result.success();
+    }
+
+    //新增员工
+    @PostMapping
+    public Result saveEmployee(@RequestBody EmployeeDTO employeeDTO){
+        log.info("员工信息：{}", employeeDTO);
+        employeeService.saveEmployee(employeeDTO);
         return Result.success();
     }
 
